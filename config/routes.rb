234899afc98990bf2650927bb8430a9036e207c0
Vixtrum <vixtrumchoo@yahoo.com>
2018-09-30 23:11:36 +0800

    Style for profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
+ 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   root 'statics#index'
 
   devise_for :users, controllers: { sessions: 'users/sessions',registrations:'users/registrations'}
   get 'users/profile' => 'users#index' , :as => "user_profile"
-
-  
 
   resources :projects do
     collection do
