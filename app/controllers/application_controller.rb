aebd8fa1af6427ccfbd407f26f4c7792de3c09af
Vixtrum <vixtrumchoo@yahoo.com>
2018-10-02 13:20:38 +0800

    text-bantu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:avatar,:username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:avatar,:username])
+        added_attrs = [:first_name, :last_name,:avatar,:username]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
       end
+     
 end
