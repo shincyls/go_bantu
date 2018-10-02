ec47bcb64e6c9d5bc2964b3e02391a9ca4193b98
Vixtrum <vixtrumchoo@yahoo.com>
2018-10-02 21:42:45 +0800

    Edit forms is down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
+  # The callback which stores the current location must be added before you authenticate the user 
+  # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
+  # before the location can be stored.
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -8,5 +12,19 @@ before_action :configure_permitted_parameters, if: :devise_controller?
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
         devise_parameter_sanitizer.permit :account_update, keys: added_attrs
       end
+
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+    #    infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
      
 end
