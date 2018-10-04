19ffe9f45e343434220fa2bd34e4b99d7b1f9af7
shincyls <shincy.mac@gmail.com>
2018-10-04 16:59:17 +0800

    fix organization displayes and unify url naming format

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,4 +1,6 @@
 class Organizer < ApplicationRecord
+    include OrganizersHelper
+
     #General Association
     belongs_to :user
 
@@ -18,4 +20,31 @@ class Organizer < ApplicationRecord
     def custom_label
         "#{self.company_name}"
     end
+
+    def url_protocol
+
+        unless !self.website_url.blank? && (self.website_url[/\Ahttp:\/\//] || self.website_url[/\Ahttps:\/\//])
+            self.website_url = "https://#{self.website_url}"
+        end
+        
+        unless !self.facebook_url.blank? && (self.facebook_url[/\Ahttp:\/\//] || self.facebook_url[/\Ahttps:\/\//])
+            self.facebook_url = "https://#{self.facebook_url}"
+        end
+
+        unless !self.instagram_url.blank? && (self.instagram_url[/\Ahttp:\/\//] || self.instagram_url[/\Ahttps:\/\//])
+            self.instagram_url = "https://#{self.instagram_url}"
+        end
+
+        unless !self.twitter_url.blank? && (self.twitter_url[/\Ahttp:\/\//] || self.twitter_url[/\Ahttps:\/\//])
+            self.twitter_url = "https://#{self.twitter_url}"
+        end
+
+        unless !self.linkedin_url.blank? && (self.linkedin_url[/\Ahttp:\/\//] || self.linkedin_url[/\Ahttps:\/\//])
+            self.linkedin_url = "https://#{self.linkedin_url}"
+        end
+
+        return self
+
+    end
+
 end
