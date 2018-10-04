98b1e60697b45d000d7f656a92dd75ad091bde7b
rdelarosa3 <rdelarosa39@gmail.com>
2018-10-04 22:51:14 +0800

    added navlink for organizer approvals and organzier helper for approvals

--- app/helpers/organizers_helper.rb
+++ app/helpers/organizers_helper.rb
@@ -1,3 +1,22 @@
 module OrganizersHelper
 
+	def project_volunteers
+		interests = []
+		organizer = current_user.organizer
+		if organizer.projects.any?
+			projects = organizer.projects
+			projects.each do |project|
+				if project.volunteer_project_joins.any? 
+					project.volunteer_project_joins.each do |volunteer|
+						if volunteer.interested?
+							interests << volunteer.id
+						end
+					end
+				end
+			end
+		end
+		
+		@volunteers = VolunteerProjectJoin.where(:id => interests)
+	end
+
 end
