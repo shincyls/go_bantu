868726e43464c6b488d14e6e488232726f368fa0
rdelarosa3 <rdelarosa39@gmail.com>
2018-10-05 17:31:51 +0800

    backspace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
   config.action_mailer.default_url_options = { host: 'gobantu.herokuapp.com' }
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
